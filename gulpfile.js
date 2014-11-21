f91c3062a8928927a22d83309ab94a60a62be483
Sam Herbert <sam@samherbert.net>
2014-11-21 16:52:23 -0500

    add minified versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var gulp   = require('gulp')
+  , insert = require('gulp-insert')
   , svgmin = require('gulp-svgmin');
 
 
-gulp.task('default', ['test']);
+gulp.task('default', ['svg:minify']);
 
 /**
  *  Minify all SVG files
@@ -12,5 +13,6 @@ gulp.task('default', ['test']);
 gulp.task('svg:minify', function () {
   return gulp.src('./svg/*.svg')
         .pipe( svgmin() )
+        .pipe( insert.prepend('<!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL -->') )
         .pipe(gulp.dest('./svg.minified'));
 });
